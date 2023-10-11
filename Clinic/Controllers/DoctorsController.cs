1207195b8d36b437469056364973ad95283cab28
object-ions <moshikoatia@gmail.com>
2023-10-11 14:33:49 -0700

    add Create and Read fx.
    Co-authored-by: Ian Bravo <bravo.ian@gmail.com>

--- Clinic/Controllers/DoctorsController.cs
+++ Clinic/Controllers/DoctorsController.cs
@@ -15,5 +15,35 @@ namespace Clinic.Controllers
     {
       _db = db;
     }
+
+    public ActionResult Index()
+    {
+      return View(_db.Doctors.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Doctor doctor)
+    {
+      _db.Doctors.Add(doctor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Doctor thisDoctor = _db.Doctors
+
+                .Include(doc => doc.JoinEntities)
+                .ThenInclude(j => j.Patient)
+                .FirstOrDefault(doc => doc.DoctorId == id);
+      return View(thisDoctor);
+    }
+
+    
   }
 }
